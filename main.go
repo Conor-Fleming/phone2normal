98678ec168147f72f1320bb0531133c76180d82a
Conor Fleming <conorfleming03@gmail.com>
2022-09-19 19:38:14 -0700

    starting phone normalizer, created DB and setting up connection

--- main.go
+++ main.go
@@ -1,8 +1,39 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"regexp"
+
+	_ "github.com/lib/pq"
 )
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "conor"
+	password = ""
+	dbname   = "sandbox"
+)
+
+func main() {
+	pq := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", pq)
+	errCheck(err)
+
+	defer db.Close()
+
+	err = db.Ping()
+	errCheck(err)
+
+	fmt.Println("Connected!")
+}
+
+func errCheck(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func normalize(number string) string {
 	//matches any char that is not a digit and replaces (line 10) with empty string
