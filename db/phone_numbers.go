b215be1a00d3cafbb7cc32b45d24e57a939818b2
Conor Fleming <conorfleming03@gmail.com>
2022-09-22 22:18:43 -0700

    refactorin..

--- db/phone_numbers.go
+++ db/phone_numbers.go
@@ -1,1 +1,130 @@
 package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+type Phone struct {
+	ID     int
+	Number string
+}
+
+type DB struct {
+	db *sql.DB
+}
+
+func Open(driver, conString string) (*DB, error) {
+	db, err := sql.Open(driver, conString)
+	if err != nil {
+		return nil, err
+	}
+	return &DB{db}, nil
+}
+
+func (db *DB) Close() error {
+	return db.db.Close()
+}
+
+func (db *DB) Ping() error {
+	return db.db.Ping()
+}
+
+func (db *DB) Insert() error {
+	phoneData := []string{
+		"(530) 514 4505",
+		"(530) 517-4585",
+		"530-514-4505",
+		"(530)5142505",
+		"1234567890",
+		"123 456 7890",
+		"123-456-7894",
+		"(123)456-7890",
+	}
+	for _, numbs := range phoneData {
+		_, err := insertNum(db.db, numbs)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func insertNum(db *sql.DB, number string) (int, error) {
+	statement := `INSERT INTO phone_numbers (number) VALUES ($1) RETURNING id;`
+	var id int
+	err := db.QueryRow(statement, number).Scan(&id)
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
+}
+
+func (db *DB) Reset() error {
+	//removing rows from table
+	command := `TRUNCATE TABLE phone_numbers RESTART IDENTITY;`
+	_, err := db.db.Exec(command)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (db *DB) GetRecords() ([]string, error) {
+	queryString := `SELECT number FROM phone_numbers;`
+	rows, err := db.db.Query(queryString)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	numberSlice := make([]string, 0)
+	for rows.Next() {
+		var number string
+		err = rows.Scan(&number)
+		if err != nil {
+			return nil, err
+		}
+		numberSlice = append(numberSlice, number)
+	}
+	return numberSlice, nil
+}
+
+func (db *DB) PrintRecords() {
+	data, err := db.GetRecords()
+	if err != nil {
+		panic(err)
+	}
+	for _, val := range data {
+		fmt.Println(val)
+	}
+
+}
+
+func (db *DB) FindPhone(newV string) error {
+	recordCheckString := `SELECT number FROM phone_numbers where number = $1;`
+	row := db.db.QueryRow(recordCheckString, newV)
+	var number string
+	err := row.Scan(&number)
+	return err
+}
+
+func (db *DB) UpdateRecord(id int, newV string) error {
+	updateString := `UPDATE phone_numbers SET number = $2 WHERE id = $1;`
+	_, err := db.db.Exec(updateString, id, newV)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (db *DB) DeleteRecord(id int) error {
+	deleteString := `DELETE FROM phone_numbers WHERE id = $1;`
+	_, err := db.db.Exec(deleteString, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"regexp"
 
+	dbPack "github.com/Conor-Fleming/phone2normal/db"
+
 	_ "github.com/lib/pq"
 )
 
@@ -19,48 +21,43 @@ const (
 func main() {
 	//establishing connection
 	pq := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	db, err := sql.Open("postgres", pq)
+	db, err := dbPack.Open("postgres", pq)
 	errCheck(err)
 
 	defer db.Close()
-
 	err = db.Ping()
 	errCheck(err)
 
 	fmt.Println("Connected!")
 
 	//removing rows at each run so I dont get the same rows over and over on each run
-	err = resetTable(db)
+	err = db.Reset()
 	errCheck(err)
 
-	//inserting some records
-	_, err = insertNum(db, "(530) 514 4505")
-	errCheck(err)
-	_, err = insertNum(db, "(530) 517-4585")
-	errCheck(err)
-	_, err = insertNum(db, "530-514-4505")
-	errCheck(err)
-	_, err = insertNum(db, "(530)5142505")
-	errCheck(err)
-	_, err = insertNum(db, "1234567890")
-	errCheck(err)
-	_, err = insertNum(db, "123 456 7890")
-	errCheck(err)
-	_, err = insertNum(db, "123-456-7894")
-	errCheck(err)
-	_, err = insertNum(db, "(123)456-7890")
-	errCheck(err)
+	db.Insert()
 
-	records, err := getRecords(db)
+	records, err := db.GetRecords()
+	db.PrintRecords()
 	errCheck(err)
 
 	fmt.Println("Normalizing....:")
-	err = normalizeRecords(db, records)
-	errCheck(err)
 
-	records, err = getRecords(db)
-	errCheck(err)
-	//fmt.Println(results)
+	for i, val := range records {
+		newVal := normalize(val)
+		err = db.FindPhone(newVal)
+		if err == nil {
+			err = db.DeleteRecord(i + 1)
+			errCheck(err)
+		} else {
+			if err != sql.ErrNoRows {
+				errCheck(err)
+			}
+			err = db.UpdateRecord(i+1, newVal)
+			errCheck(err)
+		}
+	}
+
+	db.PrintRecords()
 }
 
 func errCheck(err error) {
@@ -73,75 +70,4 @@ func normalize(number string) string {
 	//matches any char that is not a digit and replaces (line 10) with empty string
 	re := regexp.MustCompile("\\D")
 	return re.ReplaceAllString(number, "")
-}
-
-func insertNum(db *sql.DB, number string) (int, error) {
-	statement := `INSERT INTO phone_numbers (number) VALUES ($1) RETURNING id;`
-	var id int
-	err := db.QueryRow(statement, number).Scan(&id)
-	if err != nil {
-		return -1, err
-	}
-	return id, nil
-}
-
-func getRecords(db *sql.DB) ([]string, error) {
-	queryString := `SELECT number FROM phone_numbers;`
-	rows, err := db.Query(queryString)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	numberSlice := make([]string, 0)
-	for rows.Next() {
-		var number string
-		err = rows.Scan(&number)
-		if err != nil {
-			return nil, err
-		}
-		numberSlice = append(numberSlice, number)
-		fmt.Println(number)
-	}
-	return numberSlice, nil
-}
-
-func resetTable(db *sql.DB) error {
-	//removing rows from table
-	command := `TRUNCATE TABLE phone_numbers RESTART IDENTITY;`
-	_, err := db.Exec(command)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func normalizeRecords(db *sql.DB, records []string) error {
-	recordCheckString := `SELECT number FROM phone_numbers where number = $1;`
-	updateString := `UPDATE phone_numbers SET number = $2 WHERE id = $1;`
-	deleteString := `DELETE FROM phone_numbers WHERE id = $1;`
-
-	for i, v := range records {
-		newV := normalize(v)
-
-		//checking if number exists in DB
-		row := db.QueryRow(recordCheckString, newV)
-		var number string
-		err := row.Scan(&number)
-
-		//number wasnt in DB then update the row
-		if err == sql.ErrNoRows {
-			_, err := db.Exec(updateString, i+1, newV)
-			if err != nil {
-				return err
-			}
-			//else delete that row becuase its a duplicate
-		} else {
-			_, err := db.Exec(deleteString, i+1)
-			if err != nil {
-				return err
-			}
-		}
-	}
-	return nil
 }
